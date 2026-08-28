43d47ab6488ba0b5de4717cf44360f2392be9349
irhamchandra99-crypto <irhamchandra99@gmail.com>
2026-08-28 20:26:33 +0700

    Push soal part 3

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,22 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+let angka = 1;
+
+console.log();
+console.log("LOOPING PERTAMA");
+while(angka <= 10){
+    console.log(angka);
+    angka++;
+}
+
+console.log();
+console.log("LOOPING KEDUA");
+let i = 10;
+
+while(i >= 0){
+    console.log(i);
+    i--;
+}
---
+++ quiz/part3/soal5.js
@@ -0,0 +1,15 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+
+let star = "*";
+for(let i = 1; i <= input; i++){
+    console.log(star);
+    star = star + "*"
+}
